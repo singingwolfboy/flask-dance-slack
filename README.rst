6b7bf106302414c44d2ea4bfc62474ef6a7f122a
David Baumgold <david@davidbaumgold.com>
2015-12-03 18:56:28 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Flask-Dance Example App: Slack Edition
+=======================================
+
+TODO: write the README
---
+++ slack.py
@@ -0,0 +1,38 @@
+import os
+from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask, redirect, url_for
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+from raven.contrib.flask import Sentry
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+sentry = Sentry(app)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["SLACK_OAUTH_CLIENT_ID"] = os.environ.get("SLACK_OAUTH_CLIENT_ID")
+app.config["SLACK_OAUTH_CLIENT_SECRET"] = os.environ.get("SLACK_OAUTH_CLIENT_SECRET")
+slack_bp = OAuth2ConsumerBlueprint("slack", __name__,
+    base_url="https://slack.com/api/",
+    authorization_url="https://slack.com/oauth/authorize",
+    token_url="https://slack.com/api/oauth.access",
+    scope=["chat:write:bot"],
+)
+slack_bp.from_config["client_id"] = "SLACK_OAUTH_CLIENT_ID"
+slack_bp.from_config["client_secret"] = "SLACK_OAUTH_CLIENT_SECRET"
+app.register_blueprint(slack_bp, url_prefix="/login")
+
+slack = slack_bp.session
+
+@app.route("/")
+def index():
+    if not slack.authorized:
+        return redirect(url_for("slack.login"))
+    resp = slack.post("chat.postMessage", data={
+        "channel": "#general",
+        "text": "ping",
+        "emoji_icon": ":frog:",
+    })
+    assert resp.ok, resp.text
+    return "I said ping!"
+
+if __name__ == "__main__":
+    app.run()
