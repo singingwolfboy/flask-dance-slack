6b2847a71ba67f95f11dfdadbad6496741a496df
David Baumgold <david@davidbaumgold.com>
2015-12-13 12:21:37 -0500

    Add custom requests Auth object

--- slack.py
+++ slack.py
@@ -6,6 +6,7 @@ import flask
 from flask import Flask, redirect, url_for
 from flask_dance.consumer import OAuth2ConsumerBlueprint
 from raven.contrib.flask import Sentry
+from requests.auth import AuthBase
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -16,12 +17,34 @@ sentry = Sentry(app)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["SLACK_OAUTH_CLIENT_ID"] = os.environ.get("SLACK_OAUTH_CLIENT_ID")
 app.config["SLACK_OAUTH_CLIENT_SECRET"] = os.environ.get("SLACK_OAUTH_CLIENT_SECRET")
+
+
+class SlackOAuth(AuthBase):
+    """
+    Slack wants the access token to be passed in a `token` GET parameter or POST
+    parameter, rather than using the `Authorization: Bearer` header. This is
+    annoying, but we can make it work using this custom Auth object.
+    """
+    def __init__(self, blueprint):
+        self.blueprint = blueprint
+
+    def __call__(self, r):
+        if self.blueprint.token:
+            access_token = self.blueprint.token.get("access_token")
+        else:
+            access_token = None
+        if access_token:
+            r.data.setdefault('token', access_token)
+        return r
+
+
 slack_bp = OAuth2ConsumerBlueprint("slack", __name__,
     base_url="https://slack.com/api/",
     authorization_url="https://slack.com/oauth/authorize",
     token_url="https://slack.com/api/oauth.access",
     scope=["identify", "chat:write:bot"],
 )
+slack_bp.auth = SlackOAuth(slack_bp)
 slack_bp.from_config["client_id"] = "SLACK_OAUTH_CLIENT_ID"
 slack_bp.from_config["client_secret"] = "SLACK_OAUTH_CLIENT_SECRET"
 app.register_blueprint(slack_bp, url_prefix="/login")
@@ -31,14 +54,9 @@ slack = slack_bp.session
 
 @app.route("/")
 def index():
-    log.info("flask.session = %s", flask.session)
-    log.info("slack_bp.token = %s", slack_bp.token)
-    log.info("slack.token = %s", slack.token)
-    log.info("slack.authorized = %s", slack.authorized)
     if not slack.authorized:
         return redirect(url_for("slack.login"))
     resp = slack.post("chat.postMessage", data={
-        "token": slack_bp.token["access_token"],
         "channel": "#general",
         "text": "ping",
         "icon_emoji": ":robot_face:",
