4cf0658415db101b273ab28c8f2765fd5c645000
David Baumgold <david@davidbaumgold.com>
2019-01-13 19:00:34 +0100

    Remove debugging

--- slack.py
+++ slack.py
@@ -2,13 +2,9 @@ import os
 from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.slack import make_slack_blueprint, slack
-from flask_sslify import SSLify
-from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-sentry = Sentry(app)
-sslify = SSLify(app)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["SLACK_OAUTH_CLIENT_ID"] = os.environ.get("SLACK_OAUTH_CLIENT_ID")
 app.config["SLACK_OAUTH_CLIENT_SECRET"] = os.environ.get("SLACK_OAUTH_CLIENT_SECRET")
